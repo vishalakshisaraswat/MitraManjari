4627b43113f4022b802434a95e66791b023fa267
Vishalakshi Saraswat <132829055+vishalakshisaraswat@users.noreply.github.com>
2024-04-06 01:14:17 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+#include"login.h"
+using namespace login;
+
+#include"cycle.h"
+using namespace MenstrualCycle;
+
+#include"timer.h"
+using namespace cycletracker;
+
+#include"game1.h"
+using namespace game1;
+
+int main(){
+	bool exit=false;
+	//enter software
+	login::Login L;
+	login::main();
+	//authentication done!
+	
+		Timer T(28,4);
+	do{
+		
+		cout<<"\t*************"<<endl;
+		cout<<"\t1.New user? \n\t\tUpdate cycle details"<<endl;
+		cout<<"\t2. Tired?\n\t\tPlay game"<<endl;
+		cout<<"\t3. Forgot date?\n\t\tCheck cycle status"<<endl;
+		cout<<"\t4. Exit"<<endl;
+		cout<<"\tEnter your choice: "<<endl;
+		int option;
+		cin>>option;
+		switch(option){
+			case 1:
+				MenstrualCycle::main();
+				break;
+			case 2:
+				game1::main();
+				break;
+			case 3:
+				T.startCountdown();
+				break;
+			case 4:
+				system("cls");
+				exit = true;
+				cout<<"\tGood Luck! :)"<<endl;
+				Sleep(3000);
+		}
+	}
+	while(!exit);
+}
