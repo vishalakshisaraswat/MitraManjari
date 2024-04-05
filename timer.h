09085f4272d2e94fed808544f805df0dd64cf704
Vishalakshi Saraswat <132829055+vishalakshisaraswat@users.noreply.github.com>
2024-04-06 01:12:33 +0530

    timer.h

--- timer.h
+++ timer.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 #include <windows.h>
 using namespace std;
 
@@ -8,29 +9,38 @@ namespace cycletracker {
 	private:
     	int countdownStart;
     	int countdownEnd;
-
+		string ans;
+		
 	public:
     	Timer(int start, int end) : countdownStart(start), countdownEnd(end) {}
 
     	void startCountdown() {
-        	start:
+    		int f;
+        	do{
+			
         	for (int i = countdownStart; i >= 0; i--) {
             	system("cls");
     	        cout << " --------------------------------------" << endl;
         	    cout << " |                 " << i << "                  |" << endl;
             	cout << " | more days until your next period...|" << endl;
             	cout << " --------------------------------------" << endl;
-
-    	        if (i == 1) {
+            	Sleep(1500);
+			}
+			
+				
+    	        do {
         	        system("cls");
             	    cout << "-------------------------------------" << endl;
                 	cout << " | Your period may start tomorrow... |" << endl;
                 	cout << "-------------------------------------" << endl;
-                	i--;
-            	}
-            	Sleep(1500);
-        	}
-
+					cout<<"Did your period start?";
+					cin>>ans;            	
+				}
+            	while(ans =="no");
+//            	Sleep(1500);
+        	
+			if(ans =="yes"){
+			
         	for (int i = countdownEnd; i >= 0; i--) {
             	system("cls");
             	cout << "-------------------------------------" << endl;
@@ -40,7 +50,12 @@ namespace cycletracker {
 
             	Sleep(1500);
         	}
-        	goto start;
+        }
+        cout<<"\n\tKnow more... 1";
+        cout<<"\n\tExit 0";
+        cin>>f;
+        }
+        while(f);
     	}
 	};
 
